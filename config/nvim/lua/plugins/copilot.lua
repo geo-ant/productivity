d1a638a706c54cb12f970edc0269e23c32bc7c4a
geo-ant <54497890+geo-ant@users.noreply.github.com>
2023-02-25 16:23:06 +0100

    new key for copilot suggestions

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -4,4 +4,9 @@ local function init(use)
   }
 end
 
+vim.g.copilot_no_tab_map = 1
+vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.keymap.set('i', '<C-J>', '<Plug>(copilot-next)')
+vim.keymap.set('i', '<C-K>', '<Plug>(copilot-previous)')
+
 return {init = init}
