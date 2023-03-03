1fe42681092191b87c05bcd6f71579322d434780
Georgios Antonopoulos <g.ant@endiag.com>
2023-03-03 18:37:32 +0100

    l&f tweaks

--- config/nvim/lua/plugins/look_and_feel.lua
+++ config/nvim/lua/plugins/look_and_feel.lua
@@ -2,11 +2,12 @@ local function init(use)
   use { 
     "karb94/neoscroll.nvim",
     "ellisonleao/gruvbox.nvim",
+    "jnurmine/Zenburn",
     "doums/darcula"
   }
 
   -- vim.o.background = "dark"
-  vim.cmd("colorscheme gruvbox")
+  vim.cmd("colorscheme darcula")
   require("neoscroll").setup()
 end
 
