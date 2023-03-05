eaf54a908ead70b184abfbbe182c5ca16825eda6
geo-ant <54497890+geo-ant@users.noreply.github.com>
2023-03-05 17:33:29 +0100

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # productivity
-This will eventually contain my dotfiles, system configurations, vim config and so on
+This will eventually contain my dotfiles, system configurations, vim config and so on.
+
+# Notes on Installation on New System
+
+## Neovim
+Before starting neovim, we need to make sure a couple of things are installed 
+and in place
+
+1. Install Neovim, preferrably from source 
+[as described here](https://github.com/neovim/neovim/wiki/Installing-Neovim).
+2. Create a symlink from the `config/nvim` directory in this repo to 
+`~/.config/nvim` on the new machine 3. For telescope.nvim to work,
+we need to install the [suggested dependencies](https://github.com/nvim-telescope/telescope.nvim),
+in particular [ripgrep](https://github.com/BurntSushi/ripgrep) and [fd](https://github.com/sharkdp/fd).
+**Note** that for fd on Ubuntu we need to rename the executable. As described in the installation
+instructions on the website.
+4. For debugging we use codelldb, which uses `lldb-vscode` which
+comes with `lldb`. We need to install the lldb package first 
+(e.g. `sudo apt install lldb` on Ubuntu). **Note** that
+the lldb-vscode executable might not be called `lldb-vscode` but something like
+`lldb-vscode-14`. We have to create a symlink so that we can access it as 
+`lldb-vscode`.
+5. For copilot to work, we need nodejs, version 16+, which is best installed
+using the node version manager [`nvm`](https://github.com/nvm-sh/nvm), which
+we also might need to install.
+6. For some of the language servers to work we need to install `python3` **and** `python3-venv`.
+7. Now we can open nvim and make it install the packages from the config.
+This usually requires running `:PackerSync`, then quitting nvim and re-running
+it until it stops complaining.
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,55 +1,55 @@
 -- see also https://github.com/Integralist/dotfiles/blob/main/.config/nvim/lua/plugins/lsp.lua
 -- and https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/
 local function init(use)
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		'simrat39/rust-tools.nvim',
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    'simrat39/rust-tools.nvim', reuqires = {"mfussenegger/nvim-dap"},
     'ziglang/zig.vim',
     'rust-lang/rust.vim'
-	}
+  }
 
-	require('mason').setup()
-	local mason_lspconfig = require('mason-lspconfig')
-	mason_lspconfig.setup({
-		ensure_installed = {
-			--"codelldb",
-			--"cpptools",
-			"marksman",
-			"rust_analyzer",
-			"lua_ls",
-			"zls",
-			"clangd",
-			"cmake",
-			"cssls",
+  require('mason').setup()
+  local mason_lspconfig = require('mason-lspconfig')
+  mason_lspconfig.setup({
+    ensure_installed = {
+      --"codelldb",
+      --"cpptools",
+      "marksman",
+      "rust_analyzer",
+      "lua_ls",
+      "zls",
+      "clangd",
+      "cmake",
+      "cssls",
       "fortls",
-		}
-	})
-	mason_lspconfig.setup_handlers({
+    }
+  })
+  mason_lspconfig.setup_handlers({
     -- this is the default function for every handler,
     -- here https://github.com/williamboman/mason.nvim/discussions/57
     -- we can see how to set up functions for individual setup_handlers
     -- DON'T forget to call the shared code, though!
-		function(server_name)
-			require("lspconfig")[server_name].setup({
-				on_attach = function(client, bufnr)
-					require("settings/shared").on_attach(client, bufnr)
-					-- require("illuminate").on_attach(client)
+    function(server_name)
+      require("lspconfig")[server_name].setup({
+        on_attach = function(client, bufnr)
+          require("settings/shared").on_attach(client, bufnr)
+          -- require("illuminate").on_attach(client)
 
-					if server_name == "terraformls" then
-						require("treesitter-terraform-doc").setup()
-					end
-				end
-			})
-		end,
+          if server_name == "terraformls" then
+            require("treesitter-terraform-doc").setup()
+          end
+        end
+      })
+    end,
     -- individual config for the fortran language server
     ["fortls"] = function()
-			require("lspconfig").fortls.setup({
-				on_attach = function(client, bufnr)
-					require("settings/shared").on_attach(client, bufnr)
-					-- require("illuminate").on_attach(client)
-				end,
+      require("lspconfig").fortls.setup({
+        on_attach = function(client, bufnr)
+          require("settings/shared").on_attach(client, bufnr)
+          -- require("illuminate").on_attach(client)
+        end,
         -- see https://fortls.fortran-lang.org/editor_integration.html#vim-neovim-gvim
         cmd = {
           'fortls',
@@ -60,66 +60,69 @@ local function init(use)
           -- '--hover_language=fortran',
           -- '--use_signature_help'
         },
-			})
+      })
     end
-	})
+  })
 
-	local rust_tools = require("rust-tools")
+  local rust_tools = require("rust-tools")
 
-	rust_tools.setup({
-		-- rust-tools options
-		tools = {
-			autoSetHints = true,
-			inlay_hints = {
-				show_parameter_hints = true,
-				parameter_hints_prefix = "<- ",
-				other_hints_prefix = "=> ",
-			},
-		},
+  rust_tools.setup({
+    -- rust-tools options
+    tools = {
+      autoSetHints = true,
+      inlay_hints = {
+        show_parameter_hints = true,
+        parameter_hints_prefix = "<- ",
+        other_hints_prefix = "=> ",
+      },
+    },
 
-		-- all the opts to send to nvim-lspconfig
-		-- these override the defaults set by rust-tools.nvim
-		--
-		-- REFERENCE:
-		-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-		-- https://rust-analyzer.github.io/manual.html#configuration
-		-- https://rust-analyzer.github.io/manual.html#features
-		--
-		-- NOTE: The configuration format is `rust-analyzer.<section>.<property>`.
-		--       <section> should be an object.
-		--       <property> should be a primitive.
-		server = {
-			on_attach = function(client, bufnr)
-				require("settings/shared").on_attach(client, bufnr)
-				-- require("illuminate").on_attach(client)
+    -- all the opts to send to nvim-lspconfig
+    -- these override the defaults set by rust-tools.nvim
+    --
+    -- REFERENCE:
+    -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+    -- https://rust-analyzer.github.io/manual.html#configuration
+    -- https://rust-analyzer.github.io/manual.html#features
+    --
+    -- NOTE: The configuration format is `rust-analyzer.<section>.<property>`.
+    --       <section> should be an object.
+    --       <property> should be a primitive.
+    server = {
+      on_attach = function(client, bufnr)
+        require("settings/shared").on_attach(client, bufnr)
+        -- require("illuminate").on_attach(client)
 
-				local bufopts = {  buffer = bufnr }
-				--vim.keymap.set('n', '<leader><leader>rr', "<Cmd>RustRunnables<CR>", bufopts)
-				vim.keymap.set('n', 'K', "<Cmd>RustHoverActions<CR>", bufopts)
-				vim.keymap.set("n", "<Leader>a", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
-			end,
-			["rust-analyzer"] = {
-				assist = {
-					importEnforceGranularity = true,
-					importPrefix = "crate"
-				},
-				cargo = {
-					allFeatures = true
-				},
-				checkOnSave = {
-					-- default: `cargo check`
-					command = "clippy",
-					allFeatures = true,
-				},
-			},
-			inlayHints = { -- NOT SURE THIS IS VALID/WORKS 😬
-			lifetimeElisionHints = {
-				enable = true,
-				useParameterNames = true
-			},
-		},
-	}
-})
+        local bufopts = {  buffer = bufnr }
+        --vim.keymap.set('n', '<leader><leader>rr', "<Cmd>RustRunnables<CR>", bufopts)
+        vim.keymap.set('n', 'K', "<Cmd>RustHoverActions<CR>", bufopts)
+        vim.keymap.set("n", "<Leader>a", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
+      end,
+      ["rust-analyzer"] = {
+        assist = {
+          importEnforceGranularity = true,
+          importPrefix = "crate"
+        },
+        cargo = {
+          allFeatures = true
+        },
+        checkOnSave = {
+          -- default: `cargo check`
+          command = "clippy",
+          allFeatures = true,
+        },
+      },
+      inlayHints = { -- NOT SURE THIS IS VALID/WORKS 😬
+        lifetimeElisionHints = {
+          enable = true,
+          useParameterNames = true
+        },
+      },
+    },
+    dap = {
+      adapter = require('dap').adapters.lldb,
+    }
+  })
 end
 
 return {init = init}
