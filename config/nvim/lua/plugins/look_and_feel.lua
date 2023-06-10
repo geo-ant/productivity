02593a8cbd9df9ec3014b80f4999948f7842a520
geo-ant <54497890+geo-ant@users.noreply.github.com>
2023-06-10 18:27:13 +0200

    more colorschemes

--- config/nvim/lua/plugins/look_and_feel.lua
+++ config/nvim/lua/plugins/look_and_feel.lua
@@ -2,7 +2,11 @@ local function init(use)
   use { 
     "karb94/neoscroll.nvim",
     "ellisonleao/gruvbox.nvim",
-    "jnurmine/Zenburn",
+    "folke/tokyonight.nvim",
+    "EdenEast/nightfox.nvim",
+    "rebelot/kanagawa.nvim",
+    "ribru17/bamboo.nvim",
+    "loctvl842/monokai-pro.nvim",
   }
 
   -- vim.o.background = "dark"
